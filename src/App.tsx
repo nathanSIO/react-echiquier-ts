a40d6d52fbcb00da2787c408df8ddb8c1b411f2b
nathanSIO <janvnathan1@gmail.com>
2023-10-11 17:07:25 +0200

    Fonction pour les Pièces mise dans un fichier à part

--- src/App.tsx
+++ src/App.tsx
@@ -1,303 +1,304 @@
 import { useState } from "react";
 import "./App.css";
-import { Piece } from "./Service/Composant/Piece";
 import { DisplayPosition } from "./Service/Display/displayPosition";
+import { SquarePieceWhite, SquarePieceBlack,nameColumn,getNameWhitePiece  } from "./Service/Composant/Piece";
+
 
 function App() {
 
   const [position, setPosition] = useState<string>("");
   let nomPiece: string;
 
-  function SquarePieceWhite(keySquare: any){
-  switch (keySquare) {
+//   function SquarePieceWhite(keySquare: any){
+//   switch (keySquare) {
 
-    case "0A" :
-      return(<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♖</div>);
+//     case "0A" :
+//       return(<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♖</div>);
 
-    case "0B" :
-// console.log("je suis la " + nomPiece)
-      return (<div style={{
-        fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }} >♘</div>);
+//     case "0B" :
+// // console.log("je suis la " + nomPiece)
+//       return (<div style={{
+//         fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }} >♘</div>);
       
-      case "0C" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♗</div>);
+//       case "0C" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♗</div>);
 
-      case "0D" : 
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♕</div>);
+//       case "0D" : 
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♕</div>);
 
-      case "0E" : 
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♔</div>);
+//       case "0E" : 
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♔</div>);
 
-      case "0F" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♗</div>);
+//       case "0F" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♗</div>);
 
-      case "0G" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♘</div>);
+//       case "0G" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♘</div>);
       
-      case "0H" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♖</div>)
+//       case "0H" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♖</div>)
 
-      case "1A" :
-      case "1B" :
-      case "1C" :
-      case "1D" :
-      case "1E" :
-      case "1F" :
-      case "1G" :
-      case "1H" :
-        return(<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♙</div>)
-    }
-    return nomPiece;
-  }
+//       case "1A" :
+//       case "1B" :
+//       case "1C" :
+//       case "1D" :
+//       case "1E" :
+//       case "1F" :
+//       case "1G" :
+//       case "1H" :
+//         return(<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♙</div>)
+//     }
+//     return nomPiece;
+//   }
 
-  function SquarePieceBlack(keySquare: any){
-    switch (keySquare) {
-      case "7A" :
-        return(<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♜</div>);
+//   function SquarePieceBlack(keySquare: any){
+//     switch (keySquare) {
+//       case "7A" :
+//         return(<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♜</div>);
 
-      case "7B" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }} >♞</div>);
+//       case "7B" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }} >♞</div>);
       
-      case "7C" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♝</div>);
+//       case "7C" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♝</div>);
 
-      case "7D" : 
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♛</div>);
+//       case "7D" : 
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♛</div>);
 
-      case "7E" : 
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♚</div>);
+//       case "7E" : 
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♚</div>);
 
-      case "7F" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♝</div>);
+//       case "7F" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♝</div>);
 
-      case "7G" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♞</div>);
+//       case "7G" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♞</div>);
       
-      case "7H" :
-        return (<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♜</div>)
+//       case "7H" :
+//         return (<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♜</div>)
 
-      case "6A" :
-      case "6B" :
-      case "6C" :
-      case "6D" :
-      case "6E" :
-      case "6F" :
-      case "6G" :
-      case "6H" :
-        return(<div style={{
-          fontSize : '75px',
-          textAlign : "center",
-          lineHeight : "0.8"
-        }}>♟</div>)
-  }
-}
+//       case "6A" :
+//       case "6B" :
+//       case "6C" :
+//       case "6D" :
+//       case "6E" :
+//       case "6F" :
+//       case "6G" :
+//       case "6H" :
+//         return(<div style={{
+//           fontSize : '75px',
+//           textAlign : "center",
+//           lineHeight : "0.8"
+//         }}>♟</div>)
+//   }
+// }
 
-  function nameColumn(col: any){
-        switch (col){
-      case 0:
-        col = "A";
-        break;
+//   function nameColumn(col: any){
+//         switch (col){
+//       case 0:
+//         col = "A";
+//         break;
       
-      case 1 :
-        col = "B"
-        break;  
+//       case 1 :
+//         col = "B"
+//         break;  
 
-      case 2:
-        col = "C";
-        break;
+//       case 2:
+//         col = "C";
+//         break;
       
-      case 3 :
-        col = "D"
-        break;
+//       case 3 :
+//         col = "D"
+//         break;
 
-      case 4:
-        col = "E";
-        break;
+//       case 4:
+//         col = "E";
+//         break;
       
-      case 5 :
-        col = "F"
-        break;
+//       case 5 :
+//         col = "F"
+//         break;
 
-      case 6 :
-        col = "G"
-        break;
+//       case 6 :
+//         col = "G"
+//         break;
 
-      case 7 :
-        col = "H"
-        break;
-    }
-    // console.log("column after" + col);
+//       case 7 :
+//         col = "H"
+//         break;
+//     }
+//     // console.log("column after" + col);
 
-    return col ;      
+//     return col ;      
 
-  } 
+//   } 
 
-  function getNameWhitePiece(keySquare: any){
-    switch (keySquare) {
+//   function getNameWhitePiece(keySquare: any){
+//     switch (keySquare) {
 
-      case "0A" :
-         nomPiece = "TOUR BLANCHE";
-         break;
+//       case "0A" :
+//          nomPiece = "TOUR BLANCHE";
+//          break;
 
-      case "0B" :
-        nomPiece = "CAVALIER BLANC";
-        break;
+//       case "0B" :
+//         nomPiece = "CAVALIER BLANC";
+//         break;
       
-      case "0C" :
-        nomPiece = "FOU BLANC";
-        break;
+//       case "0C" :
+//         nomPiece = "FOU BLANC";
+//         break;
 
-      case "0D" : 
-        nomPiece = "REINE BLANCHE";
-        break;
+//       case "0D" : 
+//         nomPiece = "REINE BLANCHE";
+//         break;
 
-      case "0E" : 
-        nomPiece = "ROI BLANC";
-        break;
+//       case "0E" : 
+//         nomPiece = "ROI BLANC";
+//         break;
 
-      case "0F" :
-        nomPiece = "FOU BLANC";
-        break;
+//       case "0F" :
+//         nomPiece = "FOU BLANC";
+//         break;
 
-      case "0G" :
-        nomPiece = "CAVALIER BLANC";
-        break;
+//       case "0G" :
+//         nomPiece = "CAVALIER BLANC";
+//         break;
       
-      case "0H" :
-        nomPiece = "TOUR BLANCHE";
-        break;
+//       case "0H" :
+//         nomPiece = "TOUR BLANCHE";
+//         break;
 
-      case "1A" :
-      case "1B" :
-      case "1C" :
-      case "1D" :
-      case "1E" :
-      case "1F" :
-      case "1G" :
-      case "1H" :
-        nomPiece = "Pion Blanc"
-        break;
-    }
-    getNameBlackPiece(keySquare)
-    return nomPiece;
-  }
+//       case "1A" :
+//       case "1B" :
+//       case "1C" :
+//       case "1D" :
+//       case "1E" :
+//       case "1F" :
+//       case "1G" :
+//       case "1H" :
+//         nomPiece = "Pion Blanc"
+//         break;
+//     }
+//     getNameBlackPiece(keySquare)
+//     return nomPiece;
+//   }
 
-  function getNameBlackPiece(keySquare: any){
-    console.log("je suis la ")
-    switch (keySquare) {
-      case "7A" :
-         nomPiece = "TOUR NOIRE";
-         break;
+//   function getNameBlackPiece(keySquare: any){
+//     console.log("je suis la ")
+//     switch (keySquare) {
+//       case "7A" :
+//          nomPiece = "TOUR NOIRE";
+//          break;
 
-      case "7B" :
-        nomPiece = "CAVALIER NOIR";
-        break;
+//       case "7B" :
+//         nomPiece = "CAVALIER NOIR";
+//         break;
       
-      case "7C" :
-        nomPiece = "FOU NOIR";
-        break;
+//       case "7C" :
+//         nomPiece = "FOU NOIR";
+//         break;
 
-      case "7D" : 
-        nomPiece = "REINE NOIRE";
-        break;
+//       case "7D" : 
+//         nomPiece = "REINE NOIRE";
+//         break;
 
-      case "7E" : 
-        nomPiece = "ROI NOIR";
-        break;
+//       case "7E" : 
+//         nomPiece = "ROI NOIR";
+//         break;
 
-      case "7F" :
-        nomPiece = "FOU NOIR";
-        break;
+//       case "7F" :
+//         nomPiece = "FOU NOIR";
+//         break;
 
-      case "7G" :
-        nomPiece = "CAVALIER NOIR";
-        break;
+//       case "7G" :
+//         nomPiece = "CAVALIER NOIR";
+//         break;
       
-      case "7H" :
-        nomPiece = "TOUR NOIRE";
-        break;
+//       case "7H" :
+//         nomPiece = "TOUR NOIRE";
+//         break;
 
-      case "6A" :
-      case "6B" :
-      case "6C" :
-      case "6D" :
-      case "6E" :
-      case "6F" :
-      case "6G" :
-      case "6H" :
-        nomPiece = "Pion NOIR"
-        break;
-    }
+//       case "6A" :
+//       case "6B" :
+//       case "6C" :
+//       case "6D" :
+//       case "6E" :
+//       case "6F" :
+//       case "6G" :
+//       case "6H" :
+//         nomPiece = "Pion NOIR"
+//         break;
+//     }
 
-    return nomPiece;
-  }
+//     return nomPiece;
+//   }
 
   function generateChessBoard() {
     const chessBoard = [];
--- src/Service/Composant/Piece.tsx
+++ src/Service/Composant/Piece.tsx
@@ -1,85 +1,294 @@
-export function Piece () {
+let nomPiece : string;
 
-    interface ChessPiece {
-        type: string; 
-        color: string; 
-    }
+export function SquarePieceWhite(keySquare: any){
+    switch (keySquare) {
   
-  interface ChessSquare {
-    piece: ChessPiece | null; // La pièce actuelle sur la case
-  }
+      case "0A" :
+        return(<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♖</div>);
   
-  interface ChessBoardState {
-    squares: ChessSquare[][];
-  }
-  
-  const renderChessPieces = (boardState: ChessBoardState) => {
-    return boardState.squares.map((row, rowIndex) => (
-      <div key={rowIndex} >
-        {row.map((square, colIndex) => (
-          <div key={`${rowIndex}-${colIndex}`} >
-            {square.piece && (
-              <div
-                // className={`chess-piece ${square.piece.color}`}
-                title={`${square.piece.color} ${square.piece.type}`}
-              >
-                {getPieceSymbol(square.piece.type)}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    ));
-    
-  };
-  
-  const getPieceSymbol = (pieceType: string) => {
-    switch (pieceType) {
-      case 'Pion':
-        return '♙'; 
-      case 'Tour':
-        return '♖'; 
-      case 'Cavalier':
-        return '♘'; 
-      case 'Fou':
-        return '♗'; 
-      case 'Reine':
-        return '♕'; 
-      case 'Roi':
-        return '♔'; 
-      // default:
-      //   return '';
-    }
-  };
-  
-  // Composant principal
-  // const ChessBoard: React.FC = () => {
-  // function ChessBoard(){
-    const exampleBoardState: ChessBoardState = {
-      squares: [
+      case "0B" :
+  // console.log("je suis la " + nomPiece)
+        return (<div style={{
+          fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }} >♘</div>);
         
-        [
-          { piece: { type: 'Tour', color: 'white' } },
-          { piece: { type: 'Cavalier', color: 'white' } },
-          // { piece: { type: 'Fou', color: 'white' } },
-          // { piece: { type: 'Reine', color: 'white' } },
-          // { piece: { type: 'Roi', color: 'white' } },
-          // { piece: { type: 'Fou', color: 'white' } },
-          // { piece: { type: 'Cavalier', color: 'white' } },
-          // { piece: { type: 'Tour', color: 'white' } },
-        ],
-        
-
-      ],
-    };
+        case "0C" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♗</div>);
   
-    return (
-      <div className="chess-board">
-        {/* Appeler la fonction pour afficher les pièces du jeu d'échecs */}
-        {renderChessPieces(exampleBoardState)}
-      </div>
-    );
-  // };
-  // return ChessBoard;
+        case "0D" : 
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♕</div>);
+  
+        case "0E" : 
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♔</div>);
+  
+        case "0F" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♗</div>);
+  
+        case "0G" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♘</div>);
+        
+        case "0H" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♖</div>)
+  
+        case "1A" :
+        case "1B" :
+        case "1C" :
+        case "1D" :
+        case "1E" :
+        case "1F" :
+        case "1G" :
+        case "1H" :
+          return(<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♙</div>)
+      }
 }
-// export default Piece;
+  
+export function SquarePieceBlack(keySquare: any){
+      switch (keySquare) {
+        case "7A" :
+          return(<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♜</div>);
+  
+        case "7B" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }} >♞</div>);
+        
+        case "7C" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♝</div>);
+  
+        case "7D" : 
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♛</div>);
+  
+        case "7E" : 
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♚</div>);
+  
+        case "7F" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♝</div>);
+  
+        case "7G" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♞</div>);
+        
+        case "7H" :
+          return (<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♜</div>)
+  
+        case "6A" :
+        case "6B" :
+        case "6C" :
+        case "6D" :
+        case "6E" :
+        case "6F" :
+        case "6G" :
+        case "6H" :
+          return(<div style={{
+            fontSize : '75px',
+            textAlign : "center",
+            lineHeight : "0.8"
+          }}>♟</div>)
+    }
+}
+  
+export function nameColumn(col: any){
+          switch (col){
+        case 0:
+          col = "A";
+          break;
+        
+        case 1 :
+          col = "B"
+          break;  
+  
+        case 2:
+          col = "C";
+          break;
+        
+        case 3 :
+          col = "D"
+          break;
+  
+        case 4:
+          col = "E";
+          break;
+        
+        case 5 :
+          col = "F"
+          break;
+  
+        case 6 :
+          col = "G"
+          break;
+  
+        case 7 :
+          col = "H"
+          break;
+      }
+      return col ;      
+  
+} 
+  
+export function getNameWhitePiece(keySquare: any){
+      
+      switch (keySquare) {
+  
+        case "0A" :
+           nomPiece = "TOUR BLANCHE";
+           break;
+  
+        case "0B" :
+          nomPiece = "CAVALIER BLANC";
+          break;
+        
+        case "0C" :
+          nomPiece = "FOU BLANC";
+          break;
+  
+        case "0D" : 
+          nomPiece = "REINE BLANCHE";
+          break;
+  
+        case "0E" : 
+          nomPiece = "ROI BLANC";
+          break;
+  
+        case "0F" :
+          nomPiece = "FOU BLANC";
+          break;
+  
+        case "0G" :
+          nomPiece = "CAVALIER BLANC";
+          break;
+        
+        case "0H" :
+          nomPiece = "TOUR BLANCHE";
+          break;
+  
+        case "1A" :
+        case "1B" :
+        case "1C" :
+        case "1D" :
+        case "1E" :
+        case "1F" :
+        case "1G" :
+        case "1H" :
+          nomPiece = "Pion Blanc"
+      }
+      getNameBlackPiece(keySquare);
+      return nomPiece;
+}
+
+export function getNameBlackPiece(keySquare: any){
+      
+      // console.log("je suis la " + nomPiece + "    " + keySquare)
+      switch (keySquare) {
+        case "7A" :
+           nomPiece = "TOUR NOIRE";
+           break;
+  
+        case "7B" :
+          nomPiece = "CAVALIER NOIR";
+          break;
+        
+        case "7C" :
+          nomPiece = "FOU NOIR";
+          break;
+  
+        case "7D" : 
+          nomPiece = "REINE NOIRE";
+          break;
+  
+        case "7E" : 
+          nomPiece = "ROI NOIR";
+          break;
+  
+        case "7F" :
+          nomPiece = "FOU NOIR";
+          break;
+  
+        case "7G" :
+          nomPiece = "CAVALIER NOIR";
+          break;
+        
+        case "7H" :
+          console.log()
+          nomPiece = "TOUR NOIRE";
+          break;
+  
+        case "6A" :
+        case "6B" :
+        case "6C" :
+        case "6D" :
+        case "6E" :
+        case "6F" :
+        case "6G" :
+        case "6H" :
+          nomPiece = "Pion NOIR"
+          break;
+        // default :
+        //   nomPiece = "Case Vide"
+      }
+      // console.log("Position : " + nomPiece);
+      return nomPiece;
+}
+
